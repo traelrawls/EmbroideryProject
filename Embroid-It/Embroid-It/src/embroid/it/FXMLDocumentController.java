eeffe587f99124152a8721d7f7a1342d819d683c
Chris Park <christopherpark@eagles.ewu.edu>
2016-04-18 13:14:31 -0700

    All shapes and fills functional again
    Drag handler redundancies can probably be safely removed. Tried to
    organize methods/event handlers into groups based on use.

--- Embroid-It/Embroid-It/src/embroid/it/FXMLDocumentController.java
+++ Embroid-It/Embroid-It/src/embroid/it/FXMLDocumentController.java
@@ -5,10 +5,16 @@
  */
 package embroid.it;
 
+import ewu.embroidit.parkc.fill.A_EmbFill;
+import ewu.embroidit.parkc.fill.EmbFillRadial;
+import ewu.embroidit.parkc.fill.EmbFillTatamiRect;
 import ewu.embroidit.parkc.io.FormatPES;
 import ewu.embroidit.parkc.io.PECDecoder;
 import ewu.embroidit.parkc.pattern.EmbPattern;
 import ewu.embroidit.parkc.pattern.EmbStitch;
+import ewu.embroidit.parkc.shape.A_EmbShapeWrapper;
+import ewu.embroidit.parkc.shape.EmbShapeWrapperRadialFill;
+import ewu.embroidit.parkc.shape.EmbShapeWrapperTatamiFill;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
@@ -23,6 +29,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -51,7 +60,8 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label label;
     
-    
+    /*-----------------------------------------------------------------------*/
+    //Buttons
     /*-----------------------------------------------------------------------*/
     
     /**
@@ -62,7 +72,33 @@ public class FXMLDocumentController implements Initializable {
     {
         this.mouseDraggedGeneric = this.mouseDraggedHandlerLine;
         this.mouseReleasedGeneric = this.mouseReleasedHandlerLine;
-        setMouseHandlers();
+        this.setMouseHandlers();
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    /**
+     * Sets mouse handlers to operate in rectangle drawing mode.
+     */
+    @FXML
+    private void rectDrawingMode()
+    {
+        this.mouseDraggedGeneric = this.mouseDraggedHandlerRect;
+        this.mouseReleasedGeneric = this.mouseReleasedHandlerRect;
+        this.setMouseHandlers();
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    /**
+     * Sets mouse handlers to operate in ellipse drawing mode.
+     */
+    @FXML
+    private void ellipseDrawingMode()
+    {
+        this.mouseDraggedGeneric = this.mouseDraggedHandlerRect;
+        this.mouseReleasedGeneric = this.mouseReleasedHandlerEllipse;
+        this.setMouseHandlers();
     }
     
     /*-----------------------------------------------------------------------*/
@@ -96,6 +132,12 @@ public class FXMLDocumentController implements Initializable {
     }
     
     /*-----------------------------------------------------------------------*/
+    //END BUTTONS
+    /*-----------------------------------------------------------------------*/
+    
+    /*-----------------------------------------------------------------------*/
+    //MENU OPERATIONS
+    /*-----------------------------------------------------------------------*/
     
     /**
      * Imports a pattern from PES file, and switches to the stitch layer.
@@ -113,6 +155,186 @@ public class FXMLDocumentController implements Initializable {
             
             this.stitchFromStitchList(stitchList);
             this.buttonStitchLayerPressed();
+        }
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    @FXML
+    private void menuExportFile()
+    {
+     //Export call here
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    @FXML
+    private void menuOpenFile()
+    {
+        //Get pattern file
+        //check for save on existing work
+        //open file
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    @FXML
+    private void menuSaveFile()
+    {
+        //create file with file chooser
+        //call file manager save
+    }
+    /*-----------------------------------------------------------------------*/
+    //END MENU OPERATIONS
+    /*-----------------------------------------------------------------------*/
+    
+    
+    
+    /*-----------------------------------------------------------------------*/
+    
+    
+    /*-----------------------------------------------------------------------*/
+    //EVENT HANDLERS
+    /*-----------------------------------------------------------------------*/
+    
+    EventHandler<MouseEvent> mouseDraggedGeneric;
+    EventHandler<MouseEvent> mouseReleasedGeneric;
+    
+    EventHandler<MouseEvent> mousePressedHandler = new EventHandler<MouseEvent>()
+    {
+        @Override
+        public void handle(MouseEvent mouseEvent)
+        {    
+            startCoordX = mouseEvent.getX();
+            startCoordY = mouseEvent.getY();
+            endCoordX = mouseEvent.getX();
+            endCoordY = mouseEvent.getY();
+        }
+    };
+    
+    /*-----------------------------------------------------------------------*/
+    
+    EventHandler<MouseEvent> mouseDraggedHandlerLine = new EventHandler<MouseEvent>()
+    {
+        @Override
+        public void handle(MouseEvent mouseEvent)
+        {    
+            endCoordX = mouseEvent.getX();
+            endCoordY = mouseEvent.getY();
+        }
+    };
+        
+    /*-----------------------------------------------------------------------*/
+    
+    EventHandler<MouseEvent> mouseDraggedHandlerRect = new EventHandler<MouseEvent>()
+    {
+        //Redundant - Same as Dragged Line and Dragged Rect
+        @Override
+            public void handle(MouseEvent mouseEvent)
+            {
+                endCoordX = mouseEvent.getX();
+                endCoordY = mouseEvent.getY();                
+            }   
+    };
+    
+    /*-----------------------------------------------------------------------*/
+    
+    EventHandler<MouseEvent> mouseReleasedHandlerLine = new EventHandler<MouseEvent>()
+    {
+        @Override
+        public void handle(MouseEvent mouseEvent)
+        {    
+            stitchLayer.getGraphicsContext2D().strokeLine(startCoordX,startCoordY,endCoordX,endCoordY);
+            shapeLayer.getGraphicsContext2D().strokeLine(startCoordX,startCoordY,endCoordX, endCoordY);
+        }
+    };
+    
+    /*-----------------------------------------------------------------------*/
+    
+    EventHandler<MouseEvent> mouseReleasedHandlerRect = new EventHandler<MouseEvent>()
+    {
+       @Override
+            public void handle(MouseEvent mouseEvent)
+            {    
+                if (startCoordX <= endCoordX && startCoordY <= endCoordY)
+                {
+                    drawRectangleToCanvas(startCoordX,startCoordY,endCoordX-startCoordX,endCoordY-startCoordY);
+                }
+                else if (startCoordX > endCoordX)
+                {
+                    if (startCoordY <= endCoordY)
+                    {
+                        drawRectangleToCanvas(endCoordX,startCoordY,startCoordX-endCoordX,endCoordY-startCoordY);
+                    }
+                    else if (startCoordY > endCoordY)
+                    {
+                        drawRectangleToCanvas(endCoordX,endCoordY,startCoordX-endCoordX,startCoordY-endCoordY);
+                    }
+                }
+                else if (startCoordY > endCoordY)
+                {
+                    drawRectangleToCanvas(startCoordX,endCoordY,endCoordX-startCoordX,startCoordY-endCoordY);
+                }
+            }
+    };
+    
+    /*-----------------------------------------------------------------------*/
+    
+    EventHandler<MouseEvent> mouseReleasedHandlerEllipse = new EventHandler<MouseEvent>()
+    {
+        @Override
+        public void handle(MouseEvent mouseEvent)
+        {
+            if (startCoordX <= endCoordX && startCoordY <= endCoordY)
+            {
+                drawEllipseToCanvas(startCoordX+(endCoordX-startCoordX)/2,startCoordY+(endCoordY-startCoordY)/2,(endCoordX-startCoordX)/2,(endCoordY-startCoordY)/2);
+            }
+            else if (startCoordX > endCoordX)
+            {
+                if (startCoordY <= endCoordY)
+                {
+                    drawEllipseToCanvas(endCoordX+(startCoordX-endCoordX)/2,startCoordY+(endCoordY-startCoordY)/2,(startCoordX-endCoordX)/2,(endCoordY-startCoordY)/2);
+                }
+                else if (startCoordY > endCoordY)
+                {
+                    drawEllipseToCanvas(endCoordX+(startCoordX-endCoordX)/2,endCoordY+(startCoordY-endCoordY)/2,(startCoordX-endCoordX)/2,(startCoordY-endCoordY)/2);
+                }
+            }
+            else if (startCoordY > endCoordY)
+            {
+                drawEllipseToCanvas(startCoordX+(endCoordX-startCoordX)/2,endCoordY+(startCoordY-endCoordY)/2,(endCoordX-startCoordX)/2,(startCoordY-endCoordY)/2);
+            }  
+        }    
+    };
+    
+    /*-----------------------------------------------------------------------*/
+    //END EVENT HANDLERS
+    /*-----------------------------------------------------------------------*/
+    
+    /**
+     * Helper method for setting mouse handlers.
+     */
+    private void setMouseHandlers()
+    {
+        this.stitchLayer.setOnMousePressed(this.mousePressedHandler);
+        this.stitchLayer.setOnMouseDragged(this.mouseDraggedGeneric);
+        this.stitchLayer.setOnMouseReleased(this.mouseReleasedGeneric);
+        this.shapeLayer.setOnMousePressed(this.mousePressedHandler);
+        this.shapeLayer.setOnMouseDragged(this.mouseDraggedGeneric);
+        this.shapeLayer.setOnMouseReleased(this.mouseReleasedGeneric);
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    
+    
+    /*-----------------------------------------------------------------------*/
+    
+    private void drawLinesFromList(Canvas drawingCanvas, List<Line> lineList)
+    {
+        for (Line line : lineList)
+        {
+            drawingCanvas.getGraphicsContext2D().strokeLine(line.getStartX(),line.getStartY(),line.getEndX(),line.getEndY());  
         }
     }
     
@@ -143,63 +365,40 @@ public class FXMLDocumentController implements Initializable {
     
     /*-----------------------------------------------------------------------*/
     
-    /**
-     * Helper method for setting mouse handlers.
-     */
-    private void setMouseHandlers()
+    private void drawRectangleToCanvas(double xCoor, double yCoor, double width, double height)
     {
-        this.stitchLayer.setOnMousePressed(this.mousePressedHandler);
-        this.stitchLayer.setOnMouseDragged(this.mouseDraggedGeneric);
-        this.stitchLayer.setOnMouseReleased(this.mouseReleasedGeneric);
-        this.shapeLayer.setOnMousePressed(this.mousePressedHandler);
-        this.shapeLayer.setOnMouseDragged(this.mouseDraggedGeneric);
-        this.shapeLayer.setOnMouseReleased(this.mouseReleasedGeneric);
+        Rectangle newRect = new Rectangle();
+        newRect.setX(xCoor);
+        newRect.setY(yCoor);
+        newRect.setWidth(width);
+        newRect.setHeight(height);
+        A_EmbShapeWrapper rectWrapper = new EmbShapeWrapperTatamiFill(newRect);
+        pattern.addShape(newRect);
+        pattern.addShapeWrapper(rectWrapper);
+        A_EmbFill fillStrat = new EmbFillTatamiRect();
+        fillStrat.fillShape(rectWrapper);
+        drawLinesFromList(stitchLayer, rectWrapper.getLineList());
+        shapeLayer.getGraphicsContext2D().strokeRect(xCoor,yCoor,width,height);
     }
- 
-    /*-----------------------------------------------------------------------*/
-    //EVENT HANDLERS
+    
     /*-----------------------------------------------------------------------*/
     
-    EventHandler<MouseEvent> mouseDraggedGeneric;
-    EventHandler<MouseEvent> mouseReleasedGeneric;
-    EventHandler<MouseEvent> mousePressedHandler = new EventHandler<MouseEvent>()
+    private void drawEllipseToCanvas(double centerX, double centerY, double radiusX, double radiusY)
     {
-        @Override
-        public void handle(MouseEvent mouseEvent)
-        {    
-            startCoordX = mouseEvent.getX();
-            startCoordY = mouseEvent.getY();
-            endCoordX = mouseEvent.getX();
-            endCoordY = mouseEvent.getY();
-        }
-    };
+        Ellipse newEllipse = new Ellipse();
+        newEllipse.setCenterX(centerX);
+        newEllipse.setCenterY(centerY);
+        newEllipse.setRadiusX(radiusX);
+        newEllipse.setRadiusY(radiusY);
+        A_EmbShapeWrapper ellipseWrapper = new EmbShapeWrapperRadialFill(newEllipse);
+        A_EmbFill fillStrat = new EmbFillRadial();
+        pattern.addShape(newEllipse);
+        pattern.addShapeWrapper(ellipseWrapper);
+        fillStrat.fillShape(ellipseWrapper);
+        drawLinesFromList(stitchLayer, ellipseWrapper.getLineList());
+        shapeLayer.getGraphicsContext2D().strokeOval(centerX-radiusX,centerY-radiusY,radiusX*2,radiusY*2);    
+    }
     
-    /*-----------------------------------------------------------------------*/
-    
-    EventHandler<MouseEvent> mouseDraggedHandlerLine = new EventHandler<MouseEvent>()
-    {
-        @Override
-        public void handle(MouseEvent mouseEvent)
-        {    
-            endCoordX = mouseEvent.getX();
-            endCoordY = mouseEvent.getY();
-        }
-    };
-        
-    /*-----------------------------------------------------------------------*/
-    
-    EventHandler<MouseEvent> mouseReleasedHandlerLine = new EventHandler<MouseEvent>()
-    {
-        @Override
-        public void handle(MouseEvent mouseEvent)
-        {    
-            stitchLayer.getGraphicsContext2D().strokeLine(startCoordX,startCoordY,endCoordX,endCoordY);
-            shapeLayer.getGraphicsContext2D().strokeLine(startCoordX,startCoordY,endCoordX, endCoordY);
-        }
-    };
-    
-    /*-----------------------------------------------------------------------*/
-    //END EVENT HANDLERS
     /*-----------------------------------------------------------------------*/
     
     @Override
