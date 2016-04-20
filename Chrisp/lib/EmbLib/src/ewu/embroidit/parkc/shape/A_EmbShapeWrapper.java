e3ab42d9fd8516d9b9adb4e3eadd110ae3196ce0
Chris Park <christopherpark@eagles.ewu.edu>
2016-04-20 15:16:45 -0700

    clean library build

--- Chrisp/lib/EmbLib/src/ewu/embroidit/parkc/shape/A_EmbShapeWrapper.java
+++ Chrisp/lib/EmbLib/src/ewu/embroidit/parkc/shape/A_EmbShapeWrapper.java
@@ -9,7 +9,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
-
 /*-----------------------------------------------------------------------*/
 /**
  * Base abstract class for Shape wrappers. 
