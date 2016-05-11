202269a1428844f4457363e2a0d0ba8d05a8bfd1
Chris Park <christopherpark@eagles.ewu.edu>
2016-05-11 14:12:05 -0700

    Added EmbCommand container class
    For use with undo/redo operations

---
+++ Chrisp/lib/EmbLib/src/ewu/embroidit/parkc/util/EmbCommand.java
@@ -0,0 +1,55 @@
+package ewu.embroidit.parkc.util;
+
+/*-----------------------------------------------------------------------*/
+
+import ewu.embroidit.parkc.shape.A_EmbShapeWrapper;
+import javafx.scene.shape.Shape;
+
+/*-----------------------------------------------------------------------*/
+
+/**
+ * Stores the index, shape and wrapper corresponding to a editor action.
+ * Used for undo/redo functionality.
+ * @author Desolis (christopherpark@eagles.ewu.edu)
+ */
+public class EmbCommand
+{
+    /*-----------------------------------------------------------------------*/
+    
+    private int listIndex;
+    private Shape shape;
+    private A_EmbShapeWrapper wrapper;
+    
+    /*-----------------------------------------------------------------------*/
+    
+    /**
+     * constructs a command object with the given parameters.
+     * @param listIndex int
+     * @param wrapper A_EmbShapeWrapper
+     */
+    public EmbCommand(int listIndex, A_EmbShapeWrapper wrapper)
+    {
+        this.listIndex = listIndex;
+        this.wrapper = wrapper;
+    }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    /**
+     * Returns the list index that this action was last stored at.
+     * @return int
+     */
+    public int getListIndex()
+    { return this.listIndex; }
+    
+    /*-----------------------------------------------------------------------*/
+    
+    /**
+     * Returns the shape wrapper that corresponds to this operation.
+     * @return A_EmbShapeWrapper
+     */
+    public A_EmbShapeWrapper getWrapper()
+    { return this.wrapper; }
+    
+    /*-----------------------------------------------------------------------*/
+}
